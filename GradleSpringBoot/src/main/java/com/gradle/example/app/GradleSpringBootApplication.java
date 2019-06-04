a1720bd2f3a0e52153346ff7f4dc5cdff7f1ecde
ugandharkumar-gali <ugandharkumar.gali@allianz.com>
2019-06-04 11:29:52 +0700

    first commit

---
+++ GradleSpringBoot/src/main/java/com/gradle/example/app/GradleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gradle.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringBootApplication.class, args);
+	}
+
+}
